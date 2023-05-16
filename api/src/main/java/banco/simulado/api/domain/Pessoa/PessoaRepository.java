824a1d35a858edbfe240e6ffa57759f722305dc9
Rodrigo Neto <rodrigozrn@gmail.com>
2023-05-16 09:46:58 -0300

    Tornando mais genérico.

--- api/src/main/java/banco/simulado/api/domain/Pessoa/PessoaRepository.java
+++ api/src/main/java/banco/simulado/api/domain/Pessoa/PessoaRepository.java
@@ -8,5 +8,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PessoaRepository extends GenericoRepository<Pessoa, Long> {
-    Page<Pessoa> findAll(Specification<Pessoa> spec, Pageable pageable);
+    Page<Pessoa> findAll(Specification<Object> spec, Pageable pageable);
 }
--- api/src/main/java/banco/simulado/api/service/PessoaServiceImpl.java
+++ api/src/main/java/banco/simulado/api/service/PessoaServiceImpl.java
@@ -1,9 +1,9 @@
 package banco.simulado.api.service;
 
-import banco.simulado.api.domain.Agencia.AgenciaRepository;
+import banco.simulado.api.domain.GenericSpecifications;
 import banco.simulado.api.domain.Pessoa.PessoaRepository;
-import banco.simulado.api.domain.Pessoa.PessoaSpecifications;
 import banco.simulado.api.domain.Pessoa.Pessoa;
+import org.hibernate.query.sqm.ComparisonOperator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -21,8 +21,8 @@ public class PessoaServiceImpl implements PessoaService {
     }
     @Override
     public Page<Pessoa> getPessoasPorIdadeESobrenome(int idade, String sobrenome, Pageable pageable) {
-        Specification<Pessoa> spec = Specification.where(PessoaSpecifications.temIdade(idade))
-                .and(PessoaSpecifications.temSobrenome(sobrenome));
+        Specification<Object> spec = Specification.where(GenericSpecifications.fieldComparison("sobrenome", sobrenome, ComparisonOperator.EQUAL))
+                .and(GenericSpecifications.fieldComparison("idade", idade, ComparisonOperator.EQUAL));
         return pessoaRepository.findAll(spec, pageable);
     }
 }
